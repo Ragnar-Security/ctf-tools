98d74a65c20f506027db88345fb53d8a268e7340
WittsEnd2 <mwittner2@gmail.com>
2020-05-22 00:16:50 -0400

    Changed some of the source code to use another library

--- web/pcap_anlayzer/src/main.rs
+++ web/pcap_anlayzer/src/main.rs
@@ -1,83 +1,44 @@
-use pcap_parser::*;
-use pcap_parser::traits::PcapReaderIterator;
+// use pcap_parser::*;
+// use pcap_parser::traits::PcapReaderIterator;
+use pcap_file::pcap::PcapReader;
+use pcap_file::pcapng::PcapNgReader;
 use std::fs::File;
 use std::env;
-
-
-fn get_extension_from_filename(filename: &str) -> &str{
-    let split_string:Vec<&str> = filename.split(".").collect();
-    let extension = split_string[1];
-    return extension;
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let extension = get_extension_from_filename(file_path);
-    if extension.eq("pcap") || extension.eq("pcapng") {
-        let file = File::open(file_path).unwrap();
-        let mut num_blocks = 0; 
-        if extension.eq("pcap") {
-            let mut reader = LegacyPcapReader::new(65536, file).expect("LegacyPcapReader Not Found");
-            loop {
-                match reader.next() {
-                    Ok((offset, _block)) => {
-                        println!("got new block");
-                        num_blocks += 1;
-                        reader.consume(offset);
-                    },
-                    Err(PcapError::Eof) => break,
-                    Err(PcapError::Incomplete) => {
-                        reader.refill().unwrap();
-                    },
-                    Err(e) => panic!("error while reading: {:?}", e),
-                }
-            }
-            println!("num_blocks: {}", num_blocks);
-                 
-        } else if extension.eq("pcapng") {
-            let mut reader = PcapNGReader::new(65536, file).expect("PcapNGReader");
-            loop {
-                match reader.next() {
-                    Ok((offset, _block)) => {
-                        println!("got new block");
-                        num_blocks += 1;
-                        reader.consume(offset);
-                    },
-                    Err(PcapError::Eof) => break,
-                    Err(PcapError::Incomplete) => {
-                        reader.refill().unwrap();
-                    },
-                    Err(e) => panic!("error while reading: {:?}", e),
-                }
-            }
-            println!("num_blocks: {}", num_blocks);
-             
+    if extension.eq("pcap") {
+
+        let file_in = File::open(file_path).expect("Error opening file"); 
+    
+        let pcap_reader = PcapReader::new(file_in).unwrap();
+    
+        for pcap in pcap_reader {
+            let pcap = pcap.unwrap();
+            let pcap_header = pcap.header;
+            println!("{:?}", pcap_header);
+        }
+    } else if extension.eq("pcapng") {
+        let file_in = File::open(file_path).expect("Error opening file"); 
+        
+        let pcapng_reader = PcapNgReader::new(file_in).unwrap();
+        
+        for block in pcapng_reader {
+            let block = block.unwrap();
+            let parsed_block = block.parsed().unwrap();
         }
     }
-    // // if (extension == ".pcap" || extension == ".pcapng"){
-    //     let file = File::open(file_path).unwrap();
-    //     let mut num_blocks = 0;
-    //     let mut reader;
-    //     // if (extension == ".pcap") {
-    //         reader = LegacyPcapReader::new(65536, file).expect("LegacyPcapReader");
-    //     // } else {
-    //         reader = PcapNGReader::new(65536, file).expect("PcapNGReader");
-    //     // }
-    //     loop {
-    //         match reader.next() {
-    //             Ok((offset, _block)) => {
-    //                 println!("got new block");
-    //                 num_blocks += 1;
-    //                 reader.consume(offset);
-    //             },
-    //             Err(PcapError::Eof) => break,
-    //             Err(PcapError::Incomplete) => {
-    //                 reader.refill().unwrap();
-    //             },
-    //             Err(e) => panic!("error while reading: {:?}", e),
-    //         }
-    //     }        
-    //     println!("num_blocks: {}", num_blocks);
-    // }
+
+}
+
+fn get_extension_from_filename(filename: &str) -> &str{
+    let mut position = 1; 
+    if filename.chars().nth(0) == Some('.') {
+        position = 2;
+    }
+    let split_string:Vec<&str> = filename.split(".").collect();
+    let extension = split_string[position];
+    return extension;
 }
