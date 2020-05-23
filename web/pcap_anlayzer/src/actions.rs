46a3bd0c082373f5e12bd2511eb09788054bce9b
WittsEnd2 <mwittner2@gmail.com>
2020-05-23 00:38:54 -0400

    Added comments and added functions for pcapng

--- web/pcap_anlayzer/src/actions.rs
+++ web/pcap_anlayzer/src/actions.rs
@@ -4,6 +4,9 @@ use pcap_file::pcap::Packet;
 use pcap_file::pcapng::Block;
 use std::string::String;
 
+/**
+ * Currently we are assuming that we are either looking at TCP/UDP packets.
+ */
 
 /**
  * Finds the SRC IP_Addr for the packet
@@ -51,10 +54,48 @@ pub fn dst_ip (packet: Packet, ng: bool) -> String{
     return retStr; 
 }
 
+/**
+ * Gets src ip from pcap_ng packet
+ */
 pub fn src_ng_ip(block: Block) -> String {
-    return "";
+    let data = block.body;
+    let mut retVec = Vec::new();
+    for i in 26..=29 {
+        retVec.push(data[i]);
+    }
+    let mut retStr = String::new();
+    let mut counter = 0; 
+    for byte in retVec {
+        let byte_str = byte.to_string();
+        retStr.push_str(&byte_str);
+        if (counter != 3) {
+            retStr.push_str(".");
+        }
+        counter = counter+1;
+
+    }
+    return retStr; 
 }
 
+/**
+ * Get dst ip from pcap_ng packet
+ */
 pub fn dst_ng_ip(block:Block) -> String {
-    return "";
+    let data = block.body;
+    let mut retVec = Vec::new();
+    for i in 30..=33 {
+        retVec.push(data[i]);
+    }
+    let mut retStr = String::new();
+    let mut counter = 0; 
+    for byte in retVec {
+        let byte_str = byte.to_string();
+        retStr.push_str(&byte_str);
+        if (counter != 3) {
+            retStr.push_str(".");
+        }
+        counter = counter+1;
+
+    }
+    return retStr;
 }
